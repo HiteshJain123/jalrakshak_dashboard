6f0ea31ea7c72e79acbe77249cf47e4fefb3797c
HiteshJain123 <159475678+HiteshJain123@users.noreply.github.com>
2025-11-01 00:59:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Set up Streamlit page
+st.set_page_config(page_title="Dashboard", layout="wide")
+
+# Read the HTML file
+with open("dashboard.html", "r", encoding="utf-8") as f:
+    html_data = f.read()
+
+# Display the HTML content
+# st.components.v1.html(html_data, height=800, scrolling=False)
+
+
+st.components.v1.html(html_data, height=st.session_state.get('height', 800), scrolling=False)
+
